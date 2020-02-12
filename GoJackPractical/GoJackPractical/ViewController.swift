f69d41972f7530bd105140aae8ca08db6fbbd131
Anshul <anshul.shah@jump360.me>
2020-02-12 15:19:24 +0530

    Project created

---
+++ GoJackPractical/GoJackPractical/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoJackPractical
+//
+//  Created by admin on 12/02/20.
+//  Copyright © 2020 anshul shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
